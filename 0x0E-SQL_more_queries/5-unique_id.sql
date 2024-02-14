2976c661ee390a3ffbb408d91dec1d7f3cbbe7b0
KING-Effulgence <okaforekperekasunday@gmail.com>
2024-02-14 13:03:13 -0800

    sql

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,8 @@
+
+
+
+CREATE TABLE IF NOT EXISTS unique_id
+       (id INT DEFAULT 1,
+       UNIQUE (ID),
+       name VARCHAR(256));
+
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+
+
+
+SELECT cities.id, cities.name, states.name FROM cities
+JOIN states ON cities.state_id=states.id
+ORDER BY cities.id;
+
