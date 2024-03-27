a1c780414ddcdb08e0b2db0085414c0f2c1c8965
KING-Effulgence <okaforekperekasunday@gmail.com>
2024-03-27 09:45:12 -0700

    python object

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
+"""
+Displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+"""
 
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb as mysql
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
+    try:
+        db = mysql.connect(host='localhost', port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    except Exception:
+        print('Failed to connect to the database')
+        exit(0)
+
+    searched = argv[4]
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name = BINARY '{:s}' \
+                    ORDER BY id ASC;".format(searched))
+
+    result_query = cursor.fetchall()
+
+    for row in result_query:
         print(row)
-    cur.close()
-    db.close()
 
+    cursor.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """
-Contains State class and Base, an instance of declarative_base()
+Contains the class definition of a City
 """
-from sqlalchemy import Column, Integer, String, MetaData
-from sqlalchemy.ext.declarative import declarative_base
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
 
 
-class State(Base):
+class City(Base):
     """
-    Class with id and name attributes of each state
+    Representation of the table cities
     """
-    __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
